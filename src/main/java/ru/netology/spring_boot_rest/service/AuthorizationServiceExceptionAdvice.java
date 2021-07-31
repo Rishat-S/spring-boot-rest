14c51792e1aba6519ec7a9a9d44168433d93f607
Rishat Suleymanov <spk_irbis@mail.ru>
2021-07-31 09:51:58 +0300

    Refactoring

--- src/main/java/ru/netology/spring_boot_rest/service/AuthorizationServiceExceptionAdvice.java
+++ src/main/java/ru/netology/spring_boot_rest/service/AuthorizationServiceExceptionAdvice.java
@@ -4,19 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.netology.spring_boot_rest.exceptions.InvalidCredentials;
 import ru.netology.spring_boot_rest.exceptions.UnauthorizedUser;
 
 @ControllerAdvice
 public class AuthorizationServiceExceptionAdvice {
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidCredentials.class)
     public ResponseEntity<String> handle(InvalidCredentials e) {
 return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
     }
 
-//    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedUser.class)
     public ResponseEntity<String> handle(UnauthorizedUser e) {
         System.out.println(e.getMessage());
